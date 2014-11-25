537c7c7e0546f9e8d92a3061d0354084d2ff0a38
Gandhi <gandhimena@gmail.com>
2014-11-25 13:43:29 -0600

    Portafolio desplegable

---
+++ js/formulario.js
@@ -0,0 +1,16 @@
+var $button = $('#Work_itemFuentevida'),
+	$imgs_Fuentevida = $('#imagenes_Fuentevida'),
+	$regresar = $('#Work-regresar')
+
+function mostrarImagenesFuentevida(){
+	$imgs_Fuentevida.slideToggle();
+}
+
+function ocultarImagenesFuentevida(){
+	$imgs_Fuentevida.slideUp();
+	return false;
+}
+//Eventos
+
+$button.click(mostrarImagenesFuentevida)
+$regresar.click(ocultarImagenesFuentevida)
